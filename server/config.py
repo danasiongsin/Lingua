9583b479a066f36a287fcdd218aa0f8ae82ab0bd
Joel McCandless <mail@joelmccandless.com>
2025-12-02 15:43:03 -0500

    setup google cloud

--- server/config.py
+++ server/config.py
@@ -2,8 +2,11 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    LLM_API_KEY: str = os.getenv("LLM_API_KEY", "")
     GOOGLE_APPLICATION_CREDENTIALS: str = os.getenv("GOOGLE_APPLICATION_CREDENTIALS", "")
     DEBUG: bool = True
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 settings = Settings()
